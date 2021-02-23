8a7a2a61b113182d4e006458d928c2b2c9ea6530
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2021-02-23 16:24:24 +0100

    added first part of js code for categories page

--- script/categories.js
+++ script/categories.js
@@ -1,11 +1,40 @@
+const listButton = document.getElementById('trigger-list');
+const gridButton = document.getElementById('trigger-grid');
 const categoriesView = document.getElementById('grid-and-list');
 
-document.getElementById('trigger-list').addEventListener('click', () => {
+const wishlistArray = document.querySelectorAll('#wishlist-compare');
+const shopByButton = document.getElementById('shop-by');
+const shopByNavigation = document.querySelector('.layered-nav');
+const closeShopByButton = document.getElementById('close-shop-by');
+
+listButton.addEventListener('click', () => {
     categoriesView.classList.remove('products__grid-view');
     categoriesView.classList.add('products__list-view');
-})
 
-document.getElementById('trigger-grid').addEventListener('click', () => {
+    for (const item of wishlistArray) {
+        item.classList.remove('wishlist__and__compare');
+        item.classList.add('wishlist__list');
+    }
+});
+
+gridButton.addEventListener('click', () => {
     categoriesView.classList.remove('products__list-view');
     categoriesView.classList.add('products__grid-view');
+
+    for (const item of wishlistArray) {
+        item.classList.remove('wishlist__list');
+        item.classList.add('wishlist__and__compare');
+    }
+});
+
+shopByButton.addEventListener('click', () => {
+    shopByNavigation.style.display = 'block';
+    shopByNavigation.classList.add('layered-nav-mobile');
+    shopByNavigation.classList.remove('layered-nav');
+})
+
+closeShopByButton.addEventListener('click', () => {
+    shopByNavigation.style.display = 'none';
+    shopByNavigation.classList.add('layered-nav');
+    shopByNavigation.classList.remove('layered-nav-mobile');
 })
