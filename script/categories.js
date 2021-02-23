a929d9513a76f57732514e2750decbf7f45374b7
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2021-02-23 17:56:14 +0100

    added filter function

--- script/categories.js
+++ script/categories.js
@@ -7,6 +7,11 @@ const shopByButton = document.getElementById('shop-by');
 const shopByNavigation = document.querySelector('.layered-nav');
 const closeShopByButton = document.getElementById('close-shop-by');
 
+const filters = document.getElementById('layered-navigation');
+const filterOption = filters.querySelectorAll('a, input');
+const closeFiltersButton = document.getElementById('clear-button');
+
+//switch between grid and list view
 listButton.addEventListener('click', () => {
     categoriesView.classList.remove('products__grid-view');
     categoriesView.classList.add('products__list-view');
@@ -27,6 +32,7 @@ gridButton.addEventListener('click', () => {
     }
 });
 
+//open or close shop by filters
 shopByButton.addEventListener('click', () => {
     shopByNavigation.style.display = 'block';
     shopByNavigation.classList.add('layered-nav-mobile');
@@ -37,4 +43,30 @@ closeShopByButton.addEventListener('click', () => {
     shopByNavigation.style.display = 'none';
     shopByNavigation.classList.add('layered-nav');
     shopByNavigation.classList.remove('layered-nav-mobile');
+})
+
+//apply selected filters to categories
+filterOption.forEach(option => {
+    let selectedFilter;
+
+    option.addEventListener('click', event => {
+        if (event.target.tagName == 'a') {
+            event.preventDefault()
+            console.log(event.target.innerText)
+            selectedFilter = event.target.innerText
+        } else {
+            console.log(event.target.id)
+            selectedFilter = event.target.id
+        }
+        console.log(selectedFilter)
+
+        document.getElementById('selected').style.display = 'block';
+        document.querySelector('.selected__filter').innerHTML = selectedFilter;
+    })
+
+})
+
+closeFiltersButton.addEventListener('click', () => {
+    document.getElementById('selected').style.display = 'none';
+    document.querySelector('.selected__filter').innerHTML = '';
 })
