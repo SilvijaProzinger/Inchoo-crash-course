dad2c9b03c9ac41f3127ef2b7e3ef200062345dc
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2021-02-24 20:15:24 +0100

    modifying details for desktop view

--- script/product.js
+++ script/product.js
@@ -1,5 +1,7 @@
 const images = document.querySelectorAll('.product__img');
 let featuredImage = document.getElementById('featured');
+const details = document.querySelectorAll(".product__details__detail");
+const detailsContainer = document.querySelector('.product__details');
 
 //set clicked image as the one in the preview
 images.forEach((item, i) => {
@@ -8,3 +10,27 @@ images.forEach((item, i) => {
         featuredImage.src = item.getAttribute('src')
     })
 })
+
+//open the details tab one by one 
+details.forEach((openedDetail) => {
+  openedDetail.addEventListener("click", () => {
+    details.forEach((detail) => {
+      if (detail !== openedDetail) {
+        detail.removeAttribute("open");
+      }
+    })
+  })
+})
+
+const customizeDetails = () => {
+    const getBottomHeight = document.querySelector('.details__content').offsetHeight;
+    console.log(getBottomHeight)
+
+    if (window.innerWidth >= 1024){
+        detailsContainer.style.marginBottom = getBottomHeight + 40 +'px'
+    } 
+};
+
+window.addEventListener("resize", customizeDetails);
+window.onload = customizeDetails;
+
