2dc71175ce742efba64d8459f361c0e876173aca
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2021-04-14 15:37:47 +0200

    fixed menu on desktop and mobile and added search dropdown animation

--- script/menu.js
+++ script/menu.js
@@ -1,73 +1,79 @@
-const openMenuButton = document.getElementById('open-menu');
-const closeMenuButton = document.getElementById('close-menu');
-const menuBody = document.getElementById('menu-container');
+const openMenuButton = document.getElementById('open-menu')
+const closeMenuButton = document.getElementById('close-menu')
+const menuBody = document.getElementById('menu-container')
 const openCategory = document.querySelectorAll('#open-category')
-const openSubCategory = document.querySelectorAll('#open-subcategory');
-const closeCategory= document.querySelectorAll('#category-item');
-const subCategory = document.querySelectorAll('.sub-container');
+const openSubCategory = document.querySelectorAll('#open-subcategory')
+const closeCategory= document.querySelectorAll('#category-item')
 
 openMenuButton.addEventListener('click', () => {
-    menuBody.classList.remove('hidden');
-    menuBody.classList.add('menu-mobile');
-});
+    menuBody.classList.toggle('visualy-hidden')
+    menuBody.classList.toggle('menu-mobile')
+})
 
 closeMenuButton.addEventListener('click', () => {
-    menuBody.classList.remove('menu-mobile');
-    menuBody.classList.add('hidden');
-});
+    menuBody.classList.toggle('menu-mobile')
+    menuBody.classList.toggle('visualy-hidden')
+})
 
 //check resolution to see if menu should be for mobile or desktop 
 const mobileOrDesktopMenu = () => {
-    if (window.innerWidth >= 1024){
-        menuBody.classList.add('menu-desktop');        
-        menuBody.classList.remove('hidden');
-        menuBody.classList.remove('menu-mobile');
+    if (window.matchMedia("(min-width: 1024px)").matches){
+        menuBody.classList.add('menu-desktop')       
+        menuBody.classList.remove('visualy-hidden')
+        menuBody.classList.remove('menu-mobile')
     } else {
-        menuBody.classList.remove('menu-desktop');
-        menuBody.classList.add('hidden');
-    };
-};
+        menuBody.classList.remove('menu-desktop')
+        menuBody.classList.add('visualy-hidden')
+    }
+}
 
 window.addEventListener("resize", mobileOrDesktopMenu);
-window.onload = mobileOrDesktopMenu;
+document.addEventListener('DOMContentLoaded', function() {
+    mobileOrDesktopMenu()
+})
 
+//open or close menu categories on desktop
 openCategory.forEach(accordion => {
-    if (window.innerWidth >= 1024) {
+    if (window.matchMedia("(min-width: 1024px)").matches) {
         accordion.addEventListener('mouseenter', () => {
-            accordion.nextElementSibling.classList.add('accordion-opened');
-            accordion.querySelector('span').classList.remove('icon-chevron-down');
-            accordion.querySelector('span').classList.add('icon-chevron-up');
-        });
+            accordion.nextElementSibling.classList.add('accordion-opened')
+            accordion.querySelector('span').classList.toggle('icon-chevron-down')
+            accordion.querySelector('span').classList.toggle('icon-chevron-up')
+        })
     } else {
         accordion.addEventListener('click', () => {
-            let parentElement = accordion.nextElementSibling;
+            let parentElement = accordion.nextElementSibling
             parentElement.classList.toggle('accordion-opened')
             parentElement.querySelectorAll('.category-container').forEach(element => {
-                element.classList.toggle('accordion-opened');
-            });
-        });
-    };
-});
+                element.classList.toggle('accordion-opened')
+            })
+            accordion.querySelector('span').classList.toggle('icon-chevron-down')
+            accordion.querySelector('span').classList.toggle('icon-chevron-up')
+        })
+    }
+})
 
 closeCategory.forEach(category => {
-    if (window.innerWidth >= 1024) {
+    if (window.matchMedia("(min-width: 1024px)").matches) {
         category.addEventListener('mouseleave', () => {
             category.querySelectorAll('.desktop-containers').forEach(categoryChild => {
-                categoryChild.classList.remove('accordion-opened');
-            });
-            category.querySelector('span').classList.remove('icon-chevron-up');
-            category.querySelector('span').classList.add('icon-chevron-down');
-        });
-    };
-});
+                categoryChild.classList.remove('accordion-opened')
+            })
+            category.querySelector('span').classList.toggle('icon-chevron-up')
+            category.querySelector('span').classList.toggle('icon-chevron-down')
+        })
+    }
+})
 
+//open or close menu categories and subcategories on mobile
 openSubCategory.forEach(subcagetory => {
-    console.log('click')
-    if (window.innerWidth <= 1023){
+    const subCategory = document.querySelectorAll('.sub-container')
+    if (window.matchMedia("(max-width: 1023px)").matches){
         subcagetory.addEventListener('click', () => {
-            subcagetory.nextElementSibling.classList.toggle('accordion-opened');
-        });
-    };
-});
+            subcagetory.nextElementSibling.classList.toggle('accordion-opened')
+        })
+    }
+})
+
 
 
--- script/search.js
+++ script/search.js
@@ -1,13 +1,15 @@
-const openSearchButton = document.getElementById('search');
-const closeSearchForm = document.getElementById('close-search');
-const searchFormContainer = document.getElementById('search-container');
+const openSearchButton = document.getElementById('search')
+const closeSearchForm = document.getElementById('close-search')
+const searchFormContainer = document.getElementById('search-container')
 
 openSearchButton.addEventListener('click', () => {
-    searchFormContainer.classList.add('search__body');
-    searchFormContainer.classList.remove('search__hidden');
+    searchFormContainer.classList.toggle('search__body')
+    searchFormContainer.classList.toggle('visualy-hidden')
+    searchFormContainer.classList.toggle('search__body-visible')
 });
 
 closeSearchForm.addEventListener('click', () => {
-    searchFormContainer.classList.add('search__hidden');
-    searchFormContainer.classList.remove('search__body');
+    searchFormContainer.classList.toggle('visualy-hidden')
+    searchFormContainer.classList.toggle('search__body')
+    searchFormContainer.classList.toggle('search__body-closed')
 });
